d34876d868d6190071415b6765a73bf83fe87241
Enola <115070135+enola-dkfz@users.noreply.github.com>
2023-02-23 17:03:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -e
 
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias grep='grep --color=auto'
-alias l='ls -CF'
-alias la='ls -A'
-alias ll='ls -alF'
-alias ls='ls --color=auto'
-
 source /verbis/functions.sh
 
 verbis_defaults_main
